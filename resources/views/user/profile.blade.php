5964dc0b4b3f90ca5840b88ac65be20a81c8b506
Youssef <youssef_a_fadel@hotmail.com>
2021-06-20 07:49:25 +0200

    minor html adjustment

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -10,8 +10,10 @@
         <h3>{{ count($posts) . (count($posts) === 1 ? " Post" : " Posts") }}</h3>
         @foreach ($posts as $post)
             <div class=".post">
-                <p>{{ $post->title }} | {{ $post->created_at->diffForHumans() }} | {{ $post->likes  }} <i class="fas fa-heart"></i></p>
-                {{ count($post->comments) . (count($post->comments) === 1 ? " Comment" : " Comments") }}
+                <p>
+                    {{ $post->title }} | {{ $post->created_at->diffForHumans() }} | {{ $post->likes  }} <i class="fas fa-heart"></i><br>
+                    {{ count($post->comments) . (count($post->comments) === 1 ? " Comment" : " Comments") }}
+                </p>
             </div>
         @endforeach
         {{ $posts->links() }}
