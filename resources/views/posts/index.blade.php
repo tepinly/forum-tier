2c935c503d70bf18059f89ea4e8968c394edb617
Youssef <youssef_a_fadel@hotmail.com>
2021-06-24 05:56:07 +0200

    posts pagination links

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($posts as $post)
-        <div class="post">
-            <h5>{{ $post->title }}</h5>
-            <p>
-                {{ $post->user->name }}<br>{{ $post->created_at->diffForHumans() }} | {{ $post->likes }} <i
-                    class="fas fa-heart"></i> |
-                {{ count($post->comments) . (count($post->comments) === 1 ? ' Comment' : ' Comments') }}
-            </p>
-        </div>
-    @endforeach
+    <div id="postList">
+        @foreach ($posts as $post)
+            <div class="post">
+                <h5>{{ $post->title }}</h5>
+                <p>
+                    {{ $post->user->name }}<br>{{ $post->created_at->diffForHumans() }} | {{ $post->likes }} <i
+                        class="fas fa-heart"></i> |
+                    {{ count($post->comments) . (count($post->comments) === 1 ? ' Comment' : ' Comments') }}
+                </p>
+            </div>
+        @endforeach
+        {{ $posts->links() }}
+    </div>
 @endsection
