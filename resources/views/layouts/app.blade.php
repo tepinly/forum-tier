19dc2062d36d62949f57135150db709eaeb2fa56
Youssef <youssef_a_fadel@hotmail.com>
2021-07-04 15:51:42 +0200

    setting home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,11 +29,12 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                    {{ config('app.name', 'ForumTier') }}
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -41,7 +42,6 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\FriendController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes(['verify' => true]);
 
@@ -19,34 +16,32 @@ Route::get('/email/verify', function () {
     return view('auth.verify');
 })->middleware('auth')->name('verification.notice');
 
-
-Route::get('/home', [PostController::class, 'index'])->name('posts');
-
 Route::group(['middleware' => ['auth', 'verified']], function () {
-
+    
     // Create Post
     Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
     Route::post('/posts', [PostController::class, 'store'])->name('post.store');
-
+    
     // Read Post
     Route::get('/posts/{id}', [PostController::class, 'show'])->name('post');
-
+    
     // Update Post
     Route::get('/posts/{post_id}/edit', [PostController::class, 'edit'])->name('post.edit');
     Route::post('/posts/{post_id}', [PostController::class, 'update'])->name('post.update');
-
+    
     // Destroy Post
     Route::post('/posts/{post_id}/destroy', [PostController::class, 'destroy'])->name('post.destroy');
-
+    
     // Like Post
     Route::post('/posts/{post_id}/like', [LikeController::class, 'like'])->name('post.like');
-
+    
+    
     // Comment on Post
     Route::post('/posts/{post_id}/comment', [CommentController::class, 'create'])->name('post.comment');
-
+    
     // Delete Comment
     Route::post('/posts/{post_id}/comments/{comment_id}', [CommentController::class, 'delete'])->name('delete.comment');
-
+    
     // Fetch Comments Pagination
     Route::post('/posts/{post_id}/comments-fetch/{page}', [CommentController::class, 'fetch'])->name('post.comments.fetch');
 
@@ -56,13 +51,13 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     
     // Update User Avatar
     Route::post('/users/{user_id}/avatar', [UserController::class, 'updateAvatar'])->name('user.avatar.update');
-
+    
     // Update User Bio
     Route::post('/users/{user_id}/bio', [UserController::class, 'updateBio'])->name('user.bio.update');
-
+    
     // Unfollow User
     Route::post('/users/{user_id}/unfollow', [FriendController::class, 'unfollow'])->name('user.unfollow');
-
+    
     // Follow User
     Route::post('/users/{user_id}/follow', [FriendController::class, 'follow'])->name('user.follow');
 });
@@ -71,8 +66,8 @@ Route::group(['middleware' => ['auth', 'verified', 'admin']], function () {
     Route::get('/admin', [AdminController::class, 'controlPanel'])->name('admin.control.panel');
 });
 
-// Posts Index
-Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::get('/home', [PostController::class, 'index'])->name('posts');
+Route::get('/', [PostController::class, 'index']);
 
 // Fetch Post Pagination
 Route::post('/posts/fetch/{page}', [PosttController::class, 'postsFetch'])->name('posts.fetch');
