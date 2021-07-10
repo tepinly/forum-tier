df61640c68b0d1c5d6ac8aff55993e3df6a4e53c
Youssef <youssef_a_fadel@hotmail.com>
2021-07-10 22:50:00 +0200

    fixed following/followers

--- resources/views/user/followers.blade.php
+++ resources/views/user/followers.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
     <h2><a href="{{ route('user.profile', ['user_id' => $user->id]) }}"><</a> {{ $user->name }}'s Followers</h2>
-    <div id="followersList">
+    <div id="followersList" class="d-flex flex-wrap justify-content-start align-items-start">
         @foreach ($followers as $follower)
-            <div class="follow-panel">
-                <a href="{{ route('user.profile', ['user_id' => $follower->friend->id]) }}">
-                    <img class="follow-profile-pic mr-2" src="{{ asset($follower->friend->avatar) }}" width="50" alt="{{ asset($follower->friend->name) }}">
-                    {{ $follower->friend->name }}
+            <div class="follow-panel m-3">
+                <a href="{{ route('user.profile', ['user_id' => $follower->user->id]) }}">
+                    <img class="follow-profile-pic mr-2" src="{{ asset($follower->user->avatar) }}" width="50" alt="{{ asset($follower->user->name) }}">
+                    {{ $follower->user->name }}
                 </a>
                 <span class="mx-2"> | </span>
-                {{ count($follower->friend->posts) }} Posts
+                {{ count($follower->user->posts) }} Posts
             </div>
         @endforeach
     </div>
--- resources/views/user/following.blade.php
+++ resources/views/user/following.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <h2><a href="{{ route('user.profile', ['user_id' => $user->id]) }}"><</a> {{ $user->name }}'s Following</h2>
-    <div id="followersList">
+    <div id="followersList" class="d-flex flex-wrap justify-content-start align-items-start">
         @foreach ($followings as $following)
-            <div class="follow-panel">
+            <div class="follow-panel m-2">
                 <a href="{{ route('user.profile', ['user_id' => $following->friend->id]) }}">
-                    <img class="follow-profile-pic mr-2" src="{{ asset($following->user->avatar) }}" width="50" alt="{{ asset($following->user->name) }}">
-                    {{ $following->user->name }}
+                    <img class="follow-profile-pic mr-2" src="{{ asset($following->friend->avatar) }}" width="50" alt="{{ asset($following->friend->name) }}">
+                    {{ $following->friend->name }}
                 </a>
                 <span class="mx-2"> | </span>
                 {{ count($following->friend->posts) }} Posts
