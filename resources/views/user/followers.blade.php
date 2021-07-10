a4602563ce2f890cdb8a2256b349b0ae0c5193db
Youssef <youssef_a_fadel@hotmail.com>
2021-07-10 21:54:32 +0200

    css edits

--- resources/views/user/followers.blade.php
+++ resources/views/user/followers.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>{{ $user->name }}'s Followers</h2>
+    <h2><a href="{{ route('user.profile', ['user_id' => $user->id]) }}"><</a> {{ $user->name }}'s Followers</h2>
     <div id="followersList">
         @foreach ($followers as $follower)
             <div class="follow-panel">
-                <img src="{{ asset($follower->friend->avatar) }}" width="50" alt="{{ asset($follower->friend->name) }}">
-                <a href="{{ route('user.profile', ['user_id' => $follower->friend->id]) }}">{{ $follower->friend->name }}</a>
+                <a href="{{ route('user.profile', ['user_id' => $follower->friend->id]) }}">
+                    <img class="follow-profile-pic mr-2" src="{{ asset($follower->friend->avatar) }}" width="50" alt="{{ asset($follower->friend->name) }}">
+                    {{ $follower->friend->name }}
+                </a>
                 <span class="mx-2"> | </span>
                 {{ count($follower->friend->posts) }} Posts
             </div>
--- resources/views/user/following.blade.php
+++ resources/views/user/following.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>{{ $user->name }}'s Followings</h2>
+    <h2><a href="{{ route('user.profile', ['user_id' => $user->id]) }}"><</a> {{ $user->name }}'s Following</h2>
     <div id="followersList">
         @foreach ($followings as $following)
             <div class="follow-panel">
-                <img src="{{ asset($following->user->avatar) }}" width="50" alt="{{ asset($following->user->name) }}">
-                {{ $following->user->name }}
+                <a href="{{ route('user.profile', ['user_id' => $following->friend->id]) }}">
+                    <img class="follow-profile-pic mr-2" src="{{ asset($following->user->avatar) }}" width="50" alt="{{ asset($following->user->name) }}">
+                    {{ $following->user->name }}
+                </a>
+                <span class="mx-2"> | </span>
+                {{ count($following->friend->posts) }} Posts
             </div>
         @endforeach
     </div>
