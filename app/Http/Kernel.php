8223b3efb8b7a27f563066fa8ada42e015b15c81
Youssef <youssef_a_fadel@hotmail.com>
2021-07-11 23:38:10 +0200

    unnecessary middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,9 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-
-        // Https Protocol
-        \App\Http\Middleware\HttpsProtocol::class,
     ];
 
     /**
@@ -40,9 +37,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-
-            // Https Protocol
-            \App\Http\Middleware\HttpsProtocol::class,
         ],
 
         'api' => [
