eb80e41de811efc1fb32d4968bc9223aa200a3d5
Youssef <youssef_a_fadel@hotmail.com>
2021-07-11 23:53:26 +0200

    fixing env variable

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,7 +16,7 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && App::environment() === 'production') {
+        if (!$request->secure() && env('APP_ENV') === 'production') {
             return redirect()->secure($request->getRequestUri());
         }
 
